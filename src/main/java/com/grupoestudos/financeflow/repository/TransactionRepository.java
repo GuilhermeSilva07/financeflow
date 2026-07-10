fe831f01479f6cb2950c3a344d06e9887f39c592
Lucas-Kev1n <luks.kevin1@gmail.com>
2026-07-10 10:12:40 -0300

    feat: TransactionRepository created with JpaRepository

--- src/main/java/com/grupoestudos/financeflow/repository/TransactionRepository.java
+++ src/main/java/com/grupoestudos/financeflow/repository/TransactionRepository.java
@@ -3,7 +3,6 @@ package com.grupoestudos.financeflow.repository;
 import com.grupoestudos.financeflow.enums.Category;
 import com.grupoestudos.financeflow.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
